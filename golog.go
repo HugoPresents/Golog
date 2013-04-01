01725d65884325e389dad4b959dd94ae2f0c53aa
Rabbit_52 <rabbitzhang52@gmail.com>
2013-04-02 00:16:40 +0800

    尝试使用配置文件，但是失败鸟~ 明日继续

--- golog.go
+++ golog.go
@@ -2,9 +2,11 @@ package main
 
 import (
     "fmt"
-    "log"
+    //"log"
+    "os"
     "net/http"
     "text/template"
+    "encoding/json"
 )
 
 type data struct {
@@ -13,21 +15,33 @@ type data struct {
     Script []string
 }
 
-func test() {
-    fmt.Printf("hello Golog!\n")
-}
-
 func main() {
     // func routers
+    setting := loadSettings()
+    fmt.Printf("%v\n", setting)
+    m := setting.(map[string]interface{})
+    fmt.Printf("%v\n", m)
     for rule, funcname := range Urls {
         http.HandleFunc(rule, funcname)
     }
     // static files
+    /*
     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(settings["root"]+"/static"))))
     err := http.ListenAndServe(":8888", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
+    */
+}
+
+func loadSettings() interface{} {
+    var j interface{}
+    f, _ := os.Open("./settings.json")
+    //settings := string("")
+    buf := make([]byte, 1024)
+    n, _ := f.Read(buf)
+    json.Unmarshal(buf[:n], &j)
+    return j
 }
 
 func render(tplName string) *template.Template{
