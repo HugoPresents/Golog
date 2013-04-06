1ae899039f41da615729d4804ea3a64098266a6d
Rabbit_52 <rabbitzhang52@gmail.com>
2013-04-06 18:09:46 +0800

    BackStage initialization ~

---
+++ backstage.go
@@ -0,0 +1,34 @@
+/* backstage.go */
+package main
+
+import (
+    "net/http"
+)
+
+func b_index(w http.ResponseWriter, r *http.Request) {
+    
+}
+
+func create_post(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func create_cat(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func create_page(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func b_page(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func b_cat(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func b_comment(w http.ResponseWriter, r *http.Request) {
+
+}
